48a967c3abb84a765b134383429870cda2028936
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-18 20:44:08 -0300

    Project created

---
+++ src/test/java/com/projetosvinicius/projetoSpring/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosvinicius.projetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
