e4b3a25ec6645546f1f929ac420a486d5c2457f5
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-18 23:17:05 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/projetosvinicius/projetoSpring/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projetosvinicius.projetoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetosvinicius.projetoSpring.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
