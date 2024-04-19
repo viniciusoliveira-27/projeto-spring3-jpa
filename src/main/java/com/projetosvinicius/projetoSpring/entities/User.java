69159f2b3967b888cd3e3c4336d73eb59a67a8f1
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-19 18:36:42 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/projetosvinicius/projetoSpring/entities/User.java
+++ src/main/java/com/projetosvinicius/projetoSpring/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetosvinicius.projetoSpring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,11 +20,14 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -75,6 +81,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+		
 
 	@Override
 	public int hashCode() {
@@ -92,5 +103,6 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return id == other.id;
 	}
-		
+
+
 }
