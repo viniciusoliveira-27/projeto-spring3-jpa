61aaca764c86fd37eca6047a0d6715e37850256c
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-22 18:04:23 -0300

    Product

---
+++ src/main/java/com/projetosvinicius/projetoSpring/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.projetosvinicius.projetoSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetosvinicius.projetoSpring.entities.Product;
+import com.projetosvinicius.projetoSpring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
