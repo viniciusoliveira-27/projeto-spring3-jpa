cff86074765fb25a7c542c890748b3834a91f938
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-23 01:56:48 -0300

    OrderItem, many-to-many association with extra attributes part1

--- src/main/java/com/projetosvinicius/projetoSpring/config/TestConfig.java
+++ src/main/java/com/projetosvinicius/projetoSpring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetosvinicius.projetoSpring.entities.Category;
 import com.projetosvinicius.projetoSpring.entities.Order;
+import com.projetosvinicius.projetoSpring.entities.OrderItem;
 import com.projetosvinicius.projetoSpring.entities.Product;
 import com.projetosvinicius.projetoSpring.entities.User;
 import com.projetosvinicius.projetoSpring.entities.enums.OrderStatus;
@@ -33,6 +34,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+//	@Autowired
+//	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,6 +74,13 @@ public class TestConfig implements CommandLineRunner {
 				
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+//		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+//		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+//		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+//		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		
 
 	}
 
