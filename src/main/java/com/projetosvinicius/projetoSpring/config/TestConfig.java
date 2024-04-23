feba811f9e7df34d28c47d2cd1042df4cec2e0ac
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-23 12:29:13 -0300

    Payment, one-to-one association

--- src/main/java/com/projetosvinicius/projetoSpring/config/TestConfig.java
+++ src/main/java/com/projetosvinicius/projetoSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetosvinicius.projetoSpring.entities.Category;
 import com.projetosvinicius.projetoSpring.entities.Order;
 import com.projetosvinicius.projetoSpring.entities.OrderItem;
+import com.projetosvinicius.projetoSpring.entities.Payment;
 import com.projetosvinicius.projetoSpring.entities.Product;
 import com.projetosvinicius.projetoSpring.entities.User;
 import com.projetosvinicius.projetoSpring.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
--- src/main/java/com/projetosvinicius/projetoSpring/entities/Order.java
+++ src/main/java/com/projetosvinicius/projetoSpring/entities/Order.java
@@ -3,12 +3,11 @@ package com.projetosvinicius.projetoSpring.entities;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.projetosvinicius.projetoSpring.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +34,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -70,6 +73,15 @@ public class Order implements Serializable {
 		if (orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Set<OrderItem> getItems() {
