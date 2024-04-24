697596d87694b8e4a6111e9aa533e8591fc70991
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-04-23 22:29:51 -0300

    Exception handling - update

--- src/main/java/com/projetosvinicius/projetoSpring/services/UserService.java
+++ src/main/java/com/projetosvinicius/projetoSpring/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetosvinicius.projetoSpring.repositories.UserRepository;
 import com.projetosvinicius.projetoSpring.services.exceptions.DatabaseException;
 import com.projetosvinicius.projetoSpring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
